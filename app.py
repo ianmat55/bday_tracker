43b57e8ae0a36c82be89968e4afa59971c99f924
Ian <ianmat55@gmail.com>
2021-08-24 09:12:16 -0700

    learning wtforms

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, url_for, flash
-from wtforms import DateField
-from flask_wtf import Form
+from wtforms import DateField, validators, SubmitField, StringField
+from flask_wtf import FlaskForm, Form
 import sqlite3
 import os
 
@@ -8,8 +8,11 @@ def get_db_connect():
 	con = sqlite3.connect('database.db')
 	return con
 		
-class DateTime(Form):
-	pass 
+class Birthday(FlaskForm):
+	birthdate = DateField('date', format='%Y-%m-%d')
+	name = StringField('Full Name')
+	submit = SubmitField('submit')
+
 
 key = os.urandom(21)
 app = Flask(__name__)
@@ -18,44 +21,45 @@ app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
 	con = get_db_connect()
 	c = con.cursor()
 
-	if request.method=='POST':
-		data_name = request.form["name"]
-		data_bd = request.form["birthday"]
+	form = Birthday()
+
+	if form.validate_on_sumbit():
 
 		try:
 			c.execute('INSERT into birthdays (name, birthday) VALUES (?,?)',
-				(data_name, data_bd,))
+				(form.name, form.birthdate,))
 		except:
 			flash("duplicate entry")
 
-		#select data to display
-		c.execute("""
-		SELECT DISTINCT
-		name, birthday
-		, CASE 
-			WHEN strftime('%m',date('now')) > strftime('%m',date(birthday)) THEN strftime('%Y',date('now')) - strftime('%Y',date(birthday))
-			WHEN strftime('%m',date('now')) = strftime('%m',date(birthday)) THEN 
-				CASE 
-					WHEN strftime('%d',date('now')) >= strftime('%d',date(birthday)) THEN strftime('%Y',date('now')) - strftime('%Y',date(birthday))
-					ELSE strftime('%Y',date('now')) - strftime('%Y',date(birthday)) - 1
-				END
-			WHEN strftime('%m',date('now')) < strftime('%m',date(birthday)) THEN strftime('%Y',date('now')) - strftime('%Y',date(birthday)) - 1
+		# #select data to display
+		# c.execute("""
+		# SELECT DISTINCT
+		# name, birthday
+		# , CASE 
+		# 	WHEN strftime('%m',date('now')) > strftime('%m',date(birthday)) THEN strftime('%Y',date('now')) - strftime('%Y',date(birthday))
+		# 	WHEN strftime('%m',date('now')) = strftime('%m',date(birthday)) THEN 
+		# 		CASE 
+		# 			WHEN strftime('%d',date('now')) >= strftime('%d',date(birthday)) THEN strftime('%Y',date('now')) - strftime('%Y',date(birthday))
+		# 			ELSE strftime('%Y',date('now')) - strftime('%Y',date(birthday)) - 1
+		# 		END
+		# 	WHEN strftime('%m',date('now')) < strftime('%m',date(birthday)) THEN strftime('%Y',date('now')) - strftime('%Y',date(birthday)) - 1
 
-		END AS 'age'
+		# END AS 'age'
 
-		FROM birthdays
-		ORDER BY birthday;
-		""")
+		# FROM birthdays
+		# ORDER BY birthday;
+		# """)
 
-		test = c.fetchall()
+		# test = c.fetchall()
 
 		con.commit()
 		con.close()
 
-		return render_template('index.html', data_name=data_name, data_bd=data_bd, test=test)
+		return render_template('index.html', form=form)
 	
 	else:
 		return render_template('index.html')
